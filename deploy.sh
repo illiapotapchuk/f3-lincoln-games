068279478e654816e3fcf317a6f4d898a01a010a
Illia Potapchuk <illia.potapchuk@zoro.com>
2022-04-26 13:08:48 -0500

    deployment

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:illiapotapchuk/f3-lincoln-games.git main:gh-pages
+git push -f git@github.com:illiapotapchuk/f3-lincoln-games.git master:gh-pages
 cd -
